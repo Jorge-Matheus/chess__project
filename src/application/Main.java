730fd8cf6761973be601cb22c2614370aef6bf89
Jorge Matheus <crossfire_brasil2013@hotmail.com>
2023-09-19 08:28:50 -0300

    x

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,7 +13,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
 		while(true) {
 			try {
 				UI.clearScreen();
